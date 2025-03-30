498b4372711141784c1d4a011a6ca199b71e3ede
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-29 22:11:35 -0300

    add file db

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const sqlite3 = require('sqlite3').verbose()
 const csv = require('csv-parser')
 const fs = require('fs')
 const multer = require('multer')// para upload de arquivos
+
+const app = express()
+const port = 3000
+const db = new sqlite3.Database('./data.db')
