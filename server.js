f983661152cfc92daeea1a081dc8a001af4d7271
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-29 22:51:33 -0300

    craete TABLE

--- server.js
+++ server.js
@@ -24,36 +24,51 @@ db.run(`
     )
     `)
 
-app.post('/import_csv', upload.single('file'), (req,res) => {
-    if(!req.file) {
+app.post('/import_csv', upload.single('file'), (req, res) => {
+    if (!req.file) {
         return res.status(400).send('Nenhum arquivo CSV foi encontrado.')
     }
 
     const results = []
     fs.createReadStream(req.file.path)
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-        db.serialize(() => {
-            const stmt = db.prepare("INSERT INTO users (title,genre,year,country,publishade_at,descripition) VALUES (?, ?, ?, ?, ?, ?)")
-            results.forEach( row => {
-                stmt.run(row.title, row.genre, row.year, row.country, row.published_at, row.description, (err) => {
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            db.serialize(() => {
+                const stmt = db.prepare("INSERT INTO users (title,genre,year,country,publishade_at,descripition) VALUES (?, ?, ?, ?, ?, ?)")
+                results.forEach(row => {
+                    stmt.run(row.title, row.genre, row.year, row.country, row.published_at, row.description, (err) => {
+                        if (err) {
+                            console.error('Erro ao inserir linha:', err)
+                        }
+                    })
+                })
+                stmt.finalize()
+
+                fs.unlink(req.file.path, (err) => {
                     if (err) {
-                        console.error('Erro ao inserir linha:', err)
+                        console.error('Erro ao deletar arquivo temporário:', err);
                     }
                 })
+                res.status(201).send('Arquivo CSV importado e banco de dados populado com sucesso.')
             })
-            stmt.finalize()
-
-            fs.unlink(req.file.path, (err) => {
-                if (err) {
-                    console.error('Erro ao deletar arquivo temporário:', err);
-                  }
-            })
-            res.status(201).send('Arquivo CSV importado e banco de dados populado com sucesso.')
         })
-    })
+        .on('error', (error) => {
+            console.error('Erro ao processar CSV:', error);
+            res.status(500).send('Erro ao processar o arquivo CSV.');
+        });
 })
+
+pp.get('/users', (req, res) => {
+    db.all("SELECT id, name, email, age FROM users", [], (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).json({ error: err.message });
+      }
+      res.json(rows);
+    });
+  });
+  
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
