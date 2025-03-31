bb71f89c2e1e505b0d25591a24cb1eb542624e91
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-30 21:33:00 -0300

    commit initial1

--- segundo/2.js
+++ segundo/2.js
@@ -1,12 +1,12 @@
 let diamante = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
 let nd = ''
 let cp = 0
+
 for (let i=0;i<diamante.length;i++){
     if(diamante[i] === '.') {
         nd +=  diamante[i].slice(0,1)
         cp++
     } 
-
 }
 
 console.log(nd,cp)
