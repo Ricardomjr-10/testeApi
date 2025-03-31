cafd0ac8fe8307cf9a4e2ba08c77bc533c5ce960
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-30 21:15:45 -0300

    add nova tarefa

---
+++ segundo/2.js
@@ -0,0 +1,10 @@
+let diamante = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
+
+for (let i=0;i<diamante.length;i++){
+    if(diamante === '.') {
+        diamante.slice(1)
+    }
+
+}
+
+console.log(diamante.slice())
