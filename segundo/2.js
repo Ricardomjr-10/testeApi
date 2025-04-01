a0528c3cd58ba90e66f7ca86392d6e51924ca1f5
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-31 21:57:23 -0300

    bug

--- segundo/2.js
+++ segundo/2.js
@@ -4,15 +4,20 @@ let dd = ''
 let cp = 0
 let cc = 0
 
+diamante = diamante.replaceAll('.', '')
+
 for (let i=0;i<diamante.length;i++){
  
-    if(diamante[i] === '.') {
-       nd += diamante[i].slice(0,1)
+    if(diamante[i] + diamante[i + 1] === '<>') {
+      diamante[i] += diamante[i].replaceAll('<>', '')
         cp++
     } 
 }
 
-console.log(nd,cp)
+// console.log(nd,cp)
+
+
 console.log(diamante)
+ console.log(diamante.length)
 
 
