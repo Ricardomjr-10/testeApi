2ad0478a8905c7afdbfb52eb898ad0cc9caf2013
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-31 21:38:32 -0300

    implementado nova duncao

--- segundo/2.js
+++ segundo/2.js
@@ -1,13 +1,18 @@
 let diamante = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
 let nd = ''
+let dd = ''
 let cp = 0
+let cc = 0
 
 for (let i=0;i<diamante.length;i++){
+ 
     if(diamante[i] === '.') {
-        nd +=  diamante[i].slice(0,1)
+       nd += diamante[i].slice(0,1)
         cp++
     } 
 }
 
 console.log(nd,cp)
 console.log(diamante)
+
+
