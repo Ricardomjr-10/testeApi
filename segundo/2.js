2018853b8a4c243a226dde490c485bfb278ca9d4
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-31 21:57:44 -0300

    bug

--- segundo/2.js
+++ segundo/2.js
@@ -9,15 +9,21 @@ diamante = diamante.replaceAll('.', '')
 for (let i=0;i<diamante.length;i++){
  
     if(diamante[i] + diamante[i + 1] === '<>') {
-      diamante[i] += diamante[i].replaceAll('<>', '')
+     nd += diamante[i] + diamante[i + 1].replaceAll('<>', '')
         cp++
     } 
+    
 }
+
+let matches = diamante.match(/<>/g);
+let quantidade = matches ? matches.length : 0;
+
+console.log(`A string original tinha ${quantidade} ocorrências de '<>'.`)
 
 // console.log(nd,cp)
 
 
-console.log(diamante)
+console.log(nd)
  console.log(diamante.length)
 
 
