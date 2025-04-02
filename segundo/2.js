bd085201b0322ad199e35984891743073efd430f
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-02 20:47:33 -0300

    bug

--- segundo/2.js
+++ segundo/2.js
@@ -1,9 +1,20 @@
 let diamante = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
 let nd = ''
+let ne = ''
 let cp = 0
+let cc = 0
 
 
 diamante = diamante.replaceAll('.', '')
+
+let counter = 0
+
+do {
+    cc++
+    ne += diamante[counter] + diamante[counter + 1].replace('<>', '')
+    counter++
+} while (diamante[counter] + diamante[counter + 1] === '<>')
+    console.log(ne, cc)
 
 for (let i=0;i<diamante.length;i++){
  
