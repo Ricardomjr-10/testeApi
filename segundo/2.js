bfffad70c391b077fc562a86e981618dbf2a326f
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-02 21:12:19 -0300

    corrigido

--- segundo/2.js
+++ segundo/2.js
@@ -1,4 +1,18 @@
 let diamante = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
 diamante = diamante.replaceAll('.', '')
 
+let cDiamantes = 0
+let i = 0
+
+while (i < diamante.length - 1) {
+    if (diamante[i] + diamante[i + 1] === "<>") {
+        cDiamantes++
+        diamante = diamante.substring(0, i) + diamante.substring(i + 2)
+        i = 0
+    } else {
+        i++
+    }
+}
+
+console.log(`A quantidade de diamantes encontrados:`, cDiamantes)
 
