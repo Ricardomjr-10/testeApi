b7fc8cf4afc86c33e1e770576c417fa23a1c25a9
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-02 21:01:16 -0300

    corrigindo

--- segundo/2.js
+++ segundo/2.js
@@ -1,39 +1,4 @@
 let diamante = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
-let nd = ''
-let ne = ''
-let cp = 0
-let cc = 0
-
-
 diamante = diamante.replaceAll('.', '')
-
-let counter = 0
-
-do {
-    cc++
-    ne += diamante[counter] + diamante[counter + 1].replace('<>', '')
-    counter++
-} while (diamante[counter] + diamante[counter + 1] === '<>')
-    console.log(ne, cc)
-
-for (let i=0;i<diamante.length;i++){
- 
-    if(diamante[i] + diamante[i + 1] === '<>') {
-     nd += diamante[i] + diamante[i + 1].replaceAll('<>', '')
-        cp++
-    } 
-    
-}
-
-let matches = diamante.match(/<>/g);
-let quantidade = matches ? matches.length : 0;
-
-console.log(`A string original tinha ${quantidade} ocorrências de '<>'.`)
-
-// console.log(nd,cp)
-
-
-console.log(nd)
- console.log(diamante)
 
 
